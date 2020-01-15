bff4385ec5e25b4e0c2f3ef7501a6e582bc7c6ee
Chan Gavin <justin.coding@gmail.com>
2020-01-15 17:32:36 +0800

    添加public文件夹

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
   devtool: "source-map",
   entry: path.join(__dirname, "/src/index.tsx"),
   output: {
-    path: path.join(__dirname, "/dist"),
+    path: path.join(__dirname, "/public"),
     filename: "bundle.js"
   },
 
   devServer: {
-    contentBase: path.resolve(__dirname, "/dist"),
+    contentBase: path.resolve(__dirname, "/public"),
     port: 17839,
     compress: true,
     hot: true
@@ -43,7 +44,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: __dirname + "/index.html"
-    })
+    }),
+    new CleanWebpackPlugin()
   ],
 
   // 不开server，本地运行
