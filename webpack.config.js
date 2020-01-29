207bfdcb69434d722275cdf3d229d39315e9b8a9
gavin <justin.project@yahoo.com>
2020-01-29 19:36:04 +0800

    add babel support

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,13 @@ module.exports = {
         ]
       },
       {
+        test: /(\.jsx|\.js)$/,
+        use: {
+          loader: "babel-loader"
+        },
+        exclude: /node_modules/,
+      },
+      {
         enforce: "pre",
         test: /\.js$/,
         loader: "source-map-loader"
