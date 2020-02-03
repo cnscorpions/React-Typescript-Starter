3fef4f7384d7e7c0553503c67c3ec5c068c2ea6b
gavin <justin.project@yahoo.com>
2020-02-03 17:48:01 +0800

    添加public目录，把index.html移入

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: __dirname + "/index.html"
+      template: __dirname + "/public/index.html"
     }),
     new CleanWebpackPlugin()
   ],
